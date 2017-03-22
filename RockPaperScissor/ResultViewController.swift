82a87f3d68d58df7035c1f82ac74f9bf3d0d10a4
Manu Sharma <manusharma@Manus-Mac.local>
2017-03-22 11:43:19 +0530

    first commit

---
+++ RockPaperScissor/ResultViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ResultViewController.swift
+//  RockPaperScissor
+//
+//  Created by Manu Sharma on 09/03/17.
+//  Copyright © 2017 Udacity. All rights reserved.
+//
+
+import UIKit
+class ResultViewController : UIViewController{
+    
+}
---
+++ RockPaperScissor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RockPaperScissor
+//
+//  Created by Manu Sharma on 09/03/17.
+//  Copyright © 2017 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
